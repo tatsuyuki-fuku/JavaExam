f314a0565ebe898b72379b11461db8e709c29a65
tatsuyuki.fukushima <tatsuyuki.fukushima@rakus-partners.co.jp>
2025-07-08 17:25:05 +0900

    演習１回目変更後

--- src/jp/co/rakuspartners/java/basic/exam1/ex01.java
+++ src/jp/co/rakuspartners/java/basic/exam1/ex01.java
@@ -1,7 +1,7 @@
 package jp.co.rakuspartners.java.basic.exam1;
 
 public class Ex01 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Java");
     }
 
