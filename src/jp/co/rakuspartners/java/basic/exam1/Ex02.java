c8e74a1ed6124b04099b656541c606ea1bedb953
tatsuyuki.fukushima <tatsuyuki.fukushima@rakus-partners.co.jp>
2025-07-08 16:58:58 +0900

    演習１回目

--- src/jp/co/rakuspartners/java/basic/exam1/Ex02.java
+++ src/jp/co/rakuspartners/java/basic/exam1/Ex02.java
@@ -1,8 +1,8 @@
 package jp.co.rakuspartners.java.basic.exam1;
 
-public class Ex01 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Java");
-    }
+public class Ex02 {
 
+    public static void main(String[] args) throws Exception {
+        System.out.println("福嶋達之");
+    }
 }
