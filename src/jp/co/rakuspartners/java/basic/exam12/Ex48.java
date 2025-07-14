d8d1f450d70f9ba1d2ce3683fb193e5dc5d2fb71
tatsuyuki.fukushima <tatsuyuki.fukushima@rakus-partners.co.jp>
2025-07-14 10:34:06 +0900

    7/14朝

--- src/jp/co/rakuspartners/java/basic/exam12/Ex48.java
+++ src/jp/co/rakuspartners/java/basic/exam12/Ex48.java
@@ -2,14 +2,18 @@ package jp.co.rakuspartners.java.basic.exam12;
 
 public class Ex48 {
     public static void main(String[] args) {
-        System.out.println(amount(10));
+        amount(3);
+        System.out.println(amount(10));//ここにはメソッド内のsumが入る
     }
-
+    
+    //戻り値ありのメソッド
     static int amount (int a){
         int sum = 0;
+        System.out.println(sum);
         for(int i=1;i<=a;i++){
             sum+=i;
         }
-        return sum;
+        return sum;//ここの値が返される
+        
     }
 }
